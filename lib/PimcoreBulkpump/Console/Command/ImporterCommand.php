ab5e56c59dfaff048df96a348a3ba244542af9f3
Joris Ros <joris.ros@gmail.com>
2016-11-30 20:06:05 +0100

    Add import option

--- lib/PimcoreBulkpump/Console/Command/ImporterCommand.php
+++ lib/PimcoreBulkpump/Console/Command/ImporterCommand.php
@@ -5,6 +5,7 @@ namespace PimcoreBulkpump\Console\Command;
 use Pimcore\Console;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Input\InputOption;
 
 class ImporterCommand extends Console\AbstractCommand
 {
@@ -21,7 +22,10 @@ class ImporterCommand extends Console\AbstractCommand
     protected function configure()
     {
         $this->setName('bulkpump:import')
-            ->setDescription('Import by profile id');
+            ->setDescription('Import by profile id')
+            ->addOption('profileId', 'p', InputOption::VALUE_REQUIRED, 'The id of the profile you want to import', null);
+
+
     }
 
 
